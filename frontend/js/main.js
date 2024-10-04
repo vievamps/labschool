36d384055712239b2aec8332b17abbde6b807663
vivi <yngridholanda.contato@gmail.com>
2024-10-04 05:57:15 -0300

    first commit

---
+++ frontend/js/main.js
@@ -0,0 +1,124 @@
+const url = "http://localhost:3000/api"
+
+
+function searchTurmas() {
+    axios.get(`${url}/turmas`).then(
+        (response) => {
+            const data = response.data.result
+
+            let html = ""
+
+            for(let turmas of data){
+                html += `<tr>
+                <th scope="row">${turmas.id}</th>
+                <td>${turmas.nome}</td>
+                <td>${turmas.periodo}</td>
+                <td>${turmas.professor}</td>
+                <td><button class="btn btn-success" onclick="redirect(${turmas.id})">Editar</button></td>
+                <td><button class="btn btn-danger" onclick="deleteTurma(${turmas.id})">Excluir</button></td>
+                </tr>`
+            }
+            document.getElementById('table-body').innerHTML = html
+        }
+    ).catch(err => console.error(err))
+}
+function deleteTurma(id_turma){
+    axios.delete(`${url}/turma/${id_turma}`).then(
+        (response) => {
+            alert(response.data.result)
+            searchTurmas()
+        }
+    ).catch(err => console.error(err))
+}
+function createTurmas(){
+    let nome = document.getElementById('nomeTurma').value
+    let periodo = document.getElementById('Periodo').value
+    let select = document.getElementById('select-professor')
+    let option = select.options[select.selectedIndex].value
+
+    const data = {
+        nome: nome,
+        periodo: periodo,
+        professor: option
+    }
+    axios.post(`${url}/turma`, data, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response) => {
+        alert(response.data.result)
+        window.location.href = "http://localhost/FRONTEND/cadastroTurma.html"
+    }    
+    ).catch(err => console.error(err))
+}
+
+function redirect(id_turma){
+    window.location.href = `http://localhost/FRONTEND/atualizarTurma.html?id_turma=${id_turma}`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function readAtividades() {
+    axios.get(`${url}/atividade`).then(
+        (response) => {
+            const data = response.data.result
+
+            let html = ""
+
+            for(let atividade of data){
+                html += `<tr>
+                <th scope="row">${atividade.id}</th>
+                <td>${atividade.id_atividade}</td>
+                <td>${atividade.nome}</td>
+                <td>${atividade.descricao}</td>
+                <td>${atividade.data_entrega}</td>
+                <td>${atividade.turma_id}</td>
+                <td>${atividade.peso}</td>
+               <td><button class="btn btn-success"  onclick="redirect(${atividade.id})">Editar</button></td>
+                <td><button class="btn btn-danger" onclick="deleteAtividade(${atividade.id})">Excluir</button></td>
+                </tr>`
+            }
+            document.getElementById('atividade-body').innerHTML = html
+        }
+    ).catch(err => console.error(err))
+}
+function deleteAtividade(id_atividade){
+    axios.delete(`${url}/atividade/${id_atividade}`).then(
+        (response) => {
+            alert(response.data.result)
+            readAtividades() 
+        }
+    ).catch(err => console.error(err))
+}
--- frontend/script.js
+++ frontend/script.js
@@ -2,6 +2,7 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
@@ -9,3 +10,45 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+/* sidebar */
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const form = document.querySelector('.form-container.sign-in form');
+
+        form.addEventListener('submit', async (event) => {
+            event.preventDefault(); //
+
+            const email = form.querySelector('input[name="email"]').value;
+            const senha = form.querySelector('input[name="senha"]').value;
+
+            try {
+                const response = await fetch('/usuario', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ email, senha }),
+                });
+
+                const result = await response.json();
+
+                if (response.ok) {
+                   
+                    console.log('Login realizado com sucesso!', result);
+                
+                    window.location.href = 'http://localhost/FRONTEND/home.html'; // Altere para a página que deseja redirecionar
+                } else {
+                    // Login falhou
+                    console.error('Erro no login:', result.error);
+                    alert(result.error); 
+                }
+            } catch (error) {
+                console.error('Erro ao fazer a solicitação:', error);
+                alert('Ocorreu um erro. Tente novamente mais tarde.');
+            }
+        });
+    });
+
+
+
